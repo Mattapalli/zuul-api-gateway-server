5a5ce6b8aa4a7ec0ac43154c1ed5c6bdcc3af8c8
RMattapalli <RMattapalli@HIBAML134346.ad.harman.com>
2020-01-31 12:25:11 +0530

    adding spring cloud sleuth

--- zuul-api-gateway-server/src/main/java/com/rk/zuul/ZuulApiGatewayServerApplication.java
+++ zuul-api-gateway-server/src/main/java/com/rk/zuul/ZuulApiGatewayServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.eureka.registry.rule.AlwaysMatchInstanceStatusRule;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,6 +17,12 @@ public class ZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+
 	}
 
 }
